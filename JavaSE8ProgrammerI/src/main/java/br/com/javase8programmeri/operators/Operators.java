dfa9aee1798426e6da89becc8c9c20e6090fb155
Walmir Silva <walmir.design@gmail.com>
2018-06-27 13:44:46 -0300

    Iniciando os estudos de operadores em Java

---
+++ JavaSE8ProgrammerI/src/main/java/br/com/javase8programmeri/operators/Operators.java
@@ -0,0 +1,21 @@
+package br.com.javase8programmeri.operators;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		double a = 2.5;
+		float b = 2.5; // Não compila porque é um double
+		float c = 2.5f;
+		byte d = 128; // Estoura a capacidade do by que vai de -128 á 127
+		byte e = 127;
+		char f = 10;
+		char g -1; // Não compatívbel porque o tipo char só aceita positivo
+		char h = '1';
+		
+		
+		
+		
+
+	}
+
+}
